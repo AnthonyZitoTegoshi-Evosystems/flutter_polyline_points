ac2cb6dc4d8333102af4f6c99d3517da699e9dca
AnthonyZitoTegoshi-Evosystems <131671775+AnthonyZitoTegoshi-Evosystems@users.noreply.github.com>
2024-08-09 16:54:08 -0300

    Update network_util.dart

--- lib/src/network_util.dart
+++ lib/src/network_util.dart
@@ -23,41 +23,31 @@ class NetworkUtil {
     );
     if (response.statusCode == 200) {
       var parsedJson = json.decode(response.body);
-      if (parsedJson["status"]?.toLowerCase() == STATUS_OK &&
-          parsedJson["routes"] != null &&
-          parsedJson["routes"].isNotEmpty) {
+      if (parsedJson["status"]?.toLowerCase() == STATUS_OK && parsedJson["routes"] != null && parsedJson["routes"].isNotEmpty) {
         List<dynamic> routeList = parsedJson["routes"];
         for (var route in routeList) {
+          var polyline = '';
+          final legs = route['legs'];
+          for (var leg in legs) {
+            polyline += leg['steps'].map((e) => e['polyline']['points']).toList().join('');
+          }
           results.add(PolylineResult(
-            points: PolylineDecoder.run(route["overview_polyline"]["points"]),
+            points: PolylineDecoder.run(polyline),
             errorMessage: "",
             status: parsedJson["status"],
-            totalDistanceValue: route['legs']
-                .map((leg) => leg['distance']['value'])
-                .reduce((v1, v2) => v1 + v2),
-            distanceTexts: <String>[
-              ...route['legs'].map((leg) => leg['distance']['text'])
-            ],
-            distanceValues: <int>[
-              ...route['legs'].map((leg) => leg['distance']['value'])
-            ],
+            totalDistanceValue: route['legs'].map((leg) => leg['distance']['value']).reduce((v1, v2) => v1 + v2),
+            distanceTexts: <String>[...route['legs'].map((leg) => leg['distance']['text'])],
+            distanceValues: <int>[...route['legs'].map((leg) => leg['distance']['value'])],
             overviewPolyline: route["overview_polyline"]["points"],
-            totalDurationValue: route['legs']
-                .map((leg) => leg['duration']['value'])
-                .reduce((v1, v2) => v1 + v2),
-            durationTexts: <String>[
-              ...route['legs'].map((leg) => leg['duration']['text'])
-            ],
-            durationValues: <int>[
-              ...route['legs'].map((leg) => leg['duration']['value'])
-            ],
+            totalDurationValue: route['legs'].map((leg) => leg['duration']['value']).reduce((v1, v2) => v1 + v2),
+            durationTexts: <String>[...route['legs'].map((leg) => leg['duration']['text'])],
+            durationValues: <int>[...route['legs'].map((leg) => leg['duration']['value'])],
             endAddress: route["legs"].last['end_address'],
             startAddress: route["legs"].first['start_address'],
           ));
         }
       } else {
-        throw Exception(
-            "Unable to get route: Response ---> ${parsedJson["status"]} ");
+        throw Exception("Unable to get route: Response ---> ${parsedJson["status"]} ");
       }
     }
     return results;
